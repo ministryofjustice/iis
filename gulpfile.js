16d4c7fe26f43b72951d78f829149e6954d0d01b
Alistair Todd <atodd917@yahoo.co.uk>
2017-01-19 14:32:22 +0000

    Add browserify from gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 var gulp = require('gulp');
+var browserify = require('gulp-browserify');
+var uglify = require('gulp-uglify');
 
 gulp.task('test', function(){
 
 });
 
-gulp.task('build', function(){
-
+gulp.task('browserify', function () {
+    return gulp.src(['./client/js/*.js'])
+    .pipe(browserify())
+    .pipe(uglify())
+    .pipe(gulp.dest('./public/js'));
 });
+
+gulp.task('build', ['browserify']);
 
 gulp.task('default', ['build']);

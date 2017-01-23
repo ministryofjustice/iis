a9e4b978bfca6e4d60896c081672648c4dffe8ea
Z <Z@Zs-iMac.local>
2017-01-23 13:43:00 +0000

    pg setup

--- server.js
+++ server.js
@@ -43,3 +43,30 @@ app.listen(app.get('port'), function() {
 });
 
 module.exports = app;
+
+
+
+var pg = require('pg');
+
+// instantiate a new client
+// the client will read connection information from
+// the same environment variables used by postgres cli tools
+var client = new pg.Client(process.env.DATABASE_URL);
+
+// connect to our database
+client.connect(function (err) {
+  if (err) throw err;
+
+  // execute a query on our database
+  client.query('SELECT $1::text as name', ['brianc'], function (err, result) {
+    if (err) throw err;
+
+    // just print the result to the console
+    console.log(result.rows[0]); // outputs: { name: 'brianc' }
+
+    // disconnect the client
+    client.end(function (err) {
+      if (err) throw err;
+    });
+  });
+});

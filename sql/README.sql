2001c3ebdc3b9ef2b23c815078ce1487a518489a
amjadkl <amjadkl@btinternet.com>
2017-02-14 07:40:21 +0000

    Other: Test two factor

--- sql/README.sql
+++ sql/README.sql
@@ -5,5 +5,5 @@ README for running the Database Scripts:
 3. Run the noniisTable.sql script.
 4. Run the Data Migration - This will be either a Database Backup or a Migration from SQL Server Instance
 5. Run generateTransferDDL.sql
-6. Run the generated DDL from the last script agaisnt IIS Database
+6. Run the generated DDL from the last script agaisnt IIS Database.
 7. Login into the database with the IIS USER and determine if a Query can be selected agasint the IIS Tables and NON IIS tables.

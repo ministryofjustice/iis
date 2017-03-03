0f8479fd7d57594e29d9a51751decbef2b5229cc
Z <Z@Zs-iMac.local>
2017-03-03 11:26:44 +0000

    details view WIP

--- data/subject.js
+++ data/subject.js
@@ -6,7 +6,7 @@ module.exports = {
     details: function (id, callback){
         
         var sqlWhere = "PK_PRISON_NUMBER = @PK_PRISON_NUMBER",
-            sql = "SELECT PK_PRISON_NUMBER, INMATE_SURNAME, INMATE_FORENAME_1, INMATE_FORENAME_2, FORMAT(INMATE_BIRTH_DATE,'dd/MM/yyyy') AS DOB FROM IIS.LOSS_OF_LIBERTY WHERE " +  sqlWhere + ";",
+            sql = "SELECT PK_PRISON_NUMBER, INMATE_SURNAME, INMATE_FORENAME_1, INMATE_FORENAME_2, FORMAT(INMATE_BIRTH_DATE,'dd/MM/yyyy') AS DOB, DATEDIFF(year, INMATE_BIRTH_DATE, GETDATE()) AS AGE FROM IIS.LOSS_OF_LIBERTY WHERE " +  sqlWhere + ";",
             params = [{column: 'PK_PRISON_NUMBER', type: TYPES.VarChar, value: id}];
 
         db.getTuple(sql, params, function(err, cols){            

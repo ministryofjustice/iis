df1f1a7f78e41170316d4fbc14f7c7a35c3fe452
Z <Z@Zs-iMac.local>
2017-03-07 15:52:48 +0000

    added alias to the results page -- NOT IN SCOPE

--- data/search.js
+++ data/search.js
@@ -90,7 +90,9 @@ module.exports = {
             }
         });
    
-        var sql = "SELECT PK_PRISON_NUMBER, INMATE_SURNAME, INMATE_FORENAME_1, INMATE_FORENAME_2, FORMAT(INMATE_BIRTH_DATE,'dd/MM/yyyy') AS DOB FROM IIS.LOSS_OF_LIBERTY WHERE " +  sqlWhere;
+        var sql = "SELECT PK_PRISON_NUMBER, INMATE_SURNAME, INMATE_FORENAME_1, INMATE_FORENAME_2, FORMAT(INMATE_BIRTH_DATE,'dd/MM/yyyy') AS DOB, "
+        sql += "SUBSTRING((SELECT ',' + k.PERSON_FORENAME_1 + ' ' + PERSON_FORENAME_2 + ' ' + k.PERSON_SURNAME FROM IIS.KNOWN_AS k WHERE k.FK_PERSON_IDENTIFIER=l.FK_PERSON_IDENTIFIER FOR XML PATH('')),2,200000) AS ALIAS"
+        sql += " FROM IIS.LOSS_OF_LIBERTY l WHERE " +  sqlWhere;
         
         db.getCollection(sql, params, function(err, rows){
             if(err) return callback(err);

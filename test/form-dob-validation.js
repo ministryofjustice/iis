6d2ea826876d1af66cab0bd1a6997bd0de98dcf1
Alistair Todd <atodd917@yahoo.co.uk>
2017-03-08 13:42:49 +0000

    Add checks for error output

--- test/form-dob-validation.js
+++ test/form-dob-validation.js
@@ -6,34 +6,34 @@ let common = require('./common');
 let app = require("../server.js");
 
 
-describe('Date/Age/Age-range validation tests', function(){
+describe('Date/Age/Age-range validation tests', function() {
 
-   it('should display error if an invalid date is passed', function(){
-       return common.logInAs("someone")
+    it('should display error if an invalid date is passed', function() {
+        return common.logInAs("someone")
             .then(function(authedReq) {
                 return authedReq.post('/search/dob')
-                    .send({ dobOrAge: 'dob', dobDay: '29', dobMonth: '02', dobYear: '2017'})
+                    .send({dobOrAge: 'dob', dobDay: '29', dobMonth: '02', dobYear: '2017'})
                     .expect(200)
-                    .expect(function(res){
+                    .expect(function(res) {
                         expect(res.text).to.contain('error-summary');
                     });
             });
-   });
+    });
 
-   it('should display error if the date is in the future', function(){
-       return common.logInAs("someone")
+    it('should display error if the date is in the future', function() {
+        return common.logInAs("someone")
             .then(function(authedReq) {
                 return authedReq.post('/search/dob')
                     .send({dobOrAge: 'dob', dobDay: '20', dobMonth: '12', dobYear: '2020'})
                     .expect(200)
-                    .expect(function(res){
+                    .expect(function(res) {
                         expect(res.text).to.contain('error-summary')
                     });
             });
-   });
+    });
 
-    it('should return 302 if the date is valid', function(){
-       return common.logInAs("someone")
+    it('should return 302 if the date is valid', function() {
+        return common.logInAs("someone")
             .then(function(authedReq) {
                 return authedReq.post('/search/dob')
                     .send({dobOrAge: 'dob', dobDay: '10', dobMonth: '6', dobYear: '1960'})
@@ -41,39 +41,45 @@ describe('Date/Age/Age-range validation tests', function(){
             });
     });
 
-
-   it('should display error if the age is not a valid number', function(){
-       return common.logInAs("someone")
+    it('should display error if the age is not a valid number', function() {
+        return common.logInAs("someone")
             .then(function(authedReq) {
                 return authedReq.post('/search/dob')
                     .send({age: '-13'})
                     .expect(200)
+                    .expect(function(res) {
+                        expect(res.text).to.contain('error-summary')
+                    });
             });
     });
 
 
-    it('should display error if the age range is not valid', function(){
-       return common.logInAs("someone")
+    it('should display error if the age range is not valid', function() {
+        return common.logInAs("someone")
             .then(function(authedReq) {
                 return authedReq.post('/search/dob')
                     .send({dobOrAge: 'age', age: '33-30'})
                     .expect(200)
+                    .expect(function(res) {
+                        expect(res.text).to.contain('error-summary')
+                    });
             });
     });
 
-
-    
-    it('should display error if the age range is more than 5 years', function(){
-       return common.logInAs("someone")
+    it('should display error if the age range is more than 5 years', function() {
+        return common.logInAs("someone")
             .then(function(authedReq) {
                 return authedReq.post('/search/dob')
                     .send({dobOrAge: 'age', age: '33-40'})
                     .expect(200)
+                    .expect(function(res) {
+                        expect(res.text).to.contain('error-summary')
+                    });
             });
     });
-    
-   it('should return 302 if age range is valid', function(){
-       return common.logInAs("someone")
+
+    it('should return 302 if age range is valid', function() {
+        return common.logInAs("someone")
             .then(function(authedReq) {
                 return authedReq.post('/search/dob')
                     .send({dobOrAge: 'age', age: '30-33'})

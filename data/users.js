650507291be55fad5bc7014962d9f9ac7ed25676
Alistair Todd <atodd917@yahoo.co.uk>
2017-03-10 16:09:19 +0000

    Fix login error display, add html IDs

--- data/users.js
+++ data/users.js
@@ -4,6 +4,8 @@ let db = require('../server/db');
 let bcrypt = require('bcryptjs');
 let TYPES = require('tedious').TYPES;
 
+let logger = require('winston');
+
 module.exports = {
 
     checkUsernameAndPassword: function(loginId, pwd, callback) {
@@ -15,15 +17,18 @@ module.exports = {
         db.getTuple('SELECT pwd from NON_IIS.users WHERE login_id=@loginId;', params, function(err, cols) {
 
             if (err) {
+                logger.error('Error finding user: ' + err);
                 return callback(err);
             }
             if (cols === 0) {
+                logger.error('Error finding user: ' + err);
                 return callback(null, false);
             }
 
             let hash = cols.pwd.value;
 
             bcrypt.compare(pwd, hash, function(err, outcome) {
+                logger.info('Authentication outcome: ' + outcome);
                 return callback(null, outcome);
             });
         });
--- routes/login.js
+++ routes/login.js
@@ -4,7 +4,7 @@ let express = require('express');
 let users = require('../data/users');
 let content = require('../data/content');
 
-let logger=require('winston');
+let logger = require('winston');
 
 // eslint-disable-next-line
 let router = express.Router();
@@ -28,22 +28,22 @@ router.post('/', function(req, res, next) {
         logger.info('Missing login inputs');
         res.status(400);
         //
-        if(!req.body.disclaimer) {
+        if (!req.body.disclaimer) {
             err.items.push({disclaimer: 'You must confirm that you understand the disclaimer'});
             err.desc += '. ' + content.errMsg.LOGIN_ERROR_DISCLAIMER;
         }
-        
+
         res.render('login', {err: err, content: content.view.login});
     } else {
         users.checkUsernameAndPassword(loginId, pwd, function(oErr, ok) {
+
             if (oErr) {
-                logger.error('Error checking credentials: ' + oErr);
                 err = {
-                        title: content.errMsg.DB_ERROR,
-                        desc: content.errMsg.TRY_AGAIN
-                      };
-                
-                return res.render('login', {err: err, content: content.view.login});
+                    title: content.errMsg.DB_ERROR,
+                    desc: content.errMsg.TRY_AGAIN
+                };
+                logger.error('Error checking credentials: ' + oErr);
+                return res.render('login', {err: failedLoginErr, content: content.view.login});
             }
 
             if (ok === true) {
@@ -53,7 +53,7 @@ router.post('/', function(req, res, next) {
             } else {
                 logger.info('Failed to log in as: ' + loginId);
                 res.status(400);
-                res.render('login', {msg: msg, content: content.view.login});
+                res.render('login', {err: err, content: content.view.login});
             }
 
         });

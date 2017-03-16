b32ba0c970321e7ee517f764f09ff831c8456c3f
Alistair Todd <atodd917@yahoo.co.uk>
2017-03-16 09:14:02 +0000

    Add ID
    ID for feature tests
    
    
    Element IDs and formatting

--- data/content.js
+++ data/content.js
@@ -8,7 +8,7 @@ module.exports = {
         LOGIN_ERROR_DISCLAIMER: 'You must confirm that you understand the disclaimer',
         DB_ERROR: 'Unable to talk to the database',
         CANNOT_SUBMIT: 'There was a problem submitting the form',
-        NO_OPTION_SELECTED: 'Invalid selection - hello',
+        NO_OPTION_SELECTED: 'Invalid selection',
         INVALID_ID: 'The identifier you\'ve entered doesn\'t look right',
         ATLEAST_ONE_REQUIRED: 'All the fields cannot be empty',
         LETTERS_ONLY: 'A name mustn\'t contain space, numbers or special characters',

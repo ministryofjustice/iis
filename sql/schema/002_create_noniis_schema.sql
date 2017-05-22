9fe8f2d06e803ea7dadac13195f4929bdbcd3ad8
mattwhitfield <whitfield.mj@gmail.com>
2017-05-22 09:07:13 +0100

    simplified audit table

--- sql/schema/002_create_noniis_schema.sql
+++ sql/schema/002_create_noniis_schema.sql
@@ -1,18 +1,8 @@
-CREATE TABLE NON_IIS.Searches
-(
-    id INT PRIMARY KEY NOT NULL IDENTITY,
-    [user] VARCHAR(20),
-    details VARCHAR(max)
-)
-
 CREATE TABLE NON_IIS.Audit
 (
     id INT PRIMARY KEY NOT NULL IDENTITY,
     timestamp DATETIME2 DEFAULT getdate(),
-    [user] VARCHAR(20),
+    [user] VARCHAR(50),
     action VARCHAR(50),
-    details VARCHAR(max),
-    searchID INT,
-    CONSTRAINT Audit_Search_id_fk FOREIGN KEY (searchID) REFERENCES NON_IIS.Searches (id)
+    details VARCHAR(max)
 )
-CREATE INDEX Audit_searchID_uindex ON NON_IIS.Audit (searchID)

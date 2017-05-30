a4ac575c84ac6b5ba18141bdf42532be9b955db9
mattwhitfield <whitfield.mj@gmail.com>
2017-05-30 16:07:16 +0100

    added more button to summary

--- gulp/webpack.js
+++ gulp/webpack.js
@@ -3,11 +3,40 @@ const webpack2 = require('webpack');
 
 const gulp = require('gulp');
 
-gulp.task('webpack', function() {
+gulp.task('webpack', [
+    'webpackSearch',
+    'webpackMoreless',
+]);
+
+gulp.task('webpackSearch', function() {
     return gulp.src('./assets/javascripts/search/search.js')
         .pipe(webpackStream({
             output: {
                 filename: 'searchBundle.js'
+            },
+            module: {
+                rules: [{
+                    loader: 'babel-loader',
+                    exclude: [/node_modules/],
+                    query: {
+                        presets: [['es2015', {'loose': true}]]
+                    }
+                }]
+            },
+            externals: {
+                //  require("jquery") is external and available
+                //  on the global var jQuery
+                jquery: 'jQuery'
+            }
+        }, webpack2))
+        .pipe(gulp.dest('./public/javascripts'));
+});
+
+gulp.task('webpackMoreless', function() {
+    return gulp.src('./assets/javascripts/moreless/moreless.js')
+        .pipe(webpackStream({
+            output: {
+                filename: 'morelessBundle.js'
             },
             module: {
                 rules: [{
---
+++ test/client/morelessTest.js
@@ -0,0 +1,62 @@
+require('jsdom-global/register');
+const chai = require('chai');
+const $ = require('jquery');
+const expect = chai.expect;
+
+const initialPage = '<html>' +
+    '<ul id="prisonerInfoSummary">' +
+    '<li>item 1</li>' +
+    '<li class="initiallyHidden">item 2</li>' +
+    '<li class="initiallyHidden">item 3</li>' +
+    '</ul>' +
+    '</html>';
+
+describe('moreless', () => {
+    document.body.innerHTML = initialPage;
+    beforeEach(() => {
+        require('../../assets/javascripts/moreless/moreless');
+    });
+
+    it('should initially hide all with class of initiallyHidden', () => {
+        $.each($('#prisonerInfoSummary li'), (key, value) => {
+            if(key === 0) {
+                expect($(value).hasClass('js-hidden')).to.equal(false);
+            } else if(key === 1 || key === 2) {
+                expect($(value).hasClass('js-hidden')).to.equal(true);
+            }
+        });
+    });
+
+    it('should attach a more/less button', () => {
+        expect($('#prisonerInfoSummary li').last().html()).to.eql('<a>more</a>');
+    });
+
+    it('should change text to less on click', () => {
+        $('#moreless a').click();
+
+        expect($('#prisonerInfoSummary li').last().html()).to.eql('<a>less</a>');
+    });
+
+    it('should reveal hidden items on click', () => {
+        $.each($('#prisonerInfoSummary li'), (key, value) => {
+            if(key === 0 || key === 1 || key === 2) {
+                expect($(value).hasClass('js-hidden')).to.equal(false);
+            }
+        });
+    });
+
+    it('should change text to more on second click', () => {
+        $('#moreless a').click();
+        expect($('#prisonerInfoSummary li').last().html()).to.eql('<a>more</a>');
+    });
+
+    it('should hide hideable items on click', () => {
+        $.each($('#prisonerInfoSummary li'), (key, value) => {
+            if(key === 0) {
+                expect($(value).hasClass('js-hidden')).to.equal(false);
+            } else if(key === 1 || key === 2) {
+                expect($(value).hasClass('js-hidden')).to.equal(true);
+            }
+        });
+    });
+});

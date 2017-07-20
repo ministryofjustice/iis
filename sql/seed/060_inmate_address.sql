e29a48b2007e649580cf71f42978c66ab32cc6ee
Alistair Todd <alistair.todd@digital.justice.gov.uk>
2017-07-20 13:45:53 +0100

    Fix test data

--- sql/seed/060_inmate_address.sql
+++ sql/seed/060_inmate_address.sql
@@ -13,27 +13,28 @@ INSERT INTO IIS.INMATE_ADDRESS (
     INMATE_POSTCODE,
     INMATE_TEL_NO,
     XIDBKEY,
+    SQ_HAS_ADDRESS,
     FK_PRISON_NUMBER)
 
 VALUES
-('111', 12345671, 'KT', 'O', 'FIRST LASTA                   ', '005               ', '1, Street Road                                    ', 'Town A                         ', 'LIVERPOOL                     ', 'MERSEYSIDE', 'ENGLAND                       ', 'L11 1AB ', '0151 284 4224       ', 2586879029, 'AB111111'),
-('112', 12345672, 'LP', 'K', '                              ', '                  ', '                                                  ', 'Town B                         ', 'LIVERPOOL                     ', 'MERSEYSIDE', 'ENGLAND                       ', 'L11 2AB ', '                    ', 2586879028, 'AB111111'),
-('113', 12345673, 'BL', 'N', 'FIRST LASTC                   ', 'PROBAT            ', '3, Street Road                                    ', 'Town C                         ', 'LIVERPOOL                     ', 'MERSEYSIDE', 'ENGLAND                       ', 'L11 3AB ', '                    ', 2586879027, 'AB111111'),
-('114', 12345674, 'LI', 'H', 'FIRST LASTD                   ', 'PROBAT            ', '4, Street Road                                    ', 'Town D                         ', 'LIVERPOOL                     ', 'MERSEYSIDE', 'ENGLAND                       ', 'L11 4AB ', '                    ', 2586879026, 'AB111112'),
-('115', 12345675, 'WS', 'S', 'FIRST LASTE                   ', 'WIFE              ', '5, Street Road                                    ', 'Town E                         ', 'LIVERPOOL                     ', 'MERSEYSIDE', 'ENGLAND                       ', 'L11 5AB ', '                    ', 2586879025, 'AB111112'),
-('116', 12345676, 'PN', 'D', 'FIRST LASTF                   ', '                  ', '6, Street Road                                    ', 'Town F                         ', 'LIVERPOOL                     ', 'MERSEYSIDE', 'ENGLAND                       ', 'LA11 6AB', '0151 284 4224       ', 2586879024, 'AB111112'),
-('117', 12345676, 'PN', ' ', 'FIRST LASTG                   ', '                  ', '7, Street Road                                    ', 'Town G                         ', 'LIVERPOOL                     ', 'MERSEYSIDE', 'ENGLAND                       ', 'LA11 7AB', '0151 284 4224       ', 2586879023, 'AB111112'),
+('111', 12345671, 'KT', 'O', 'FIRST LASTA                   ', '005               ', '1, Street Road                                    ', 'Town A                         ', 'LIVERPOOL                     ', 'MERSEYSIDE', 'ENGLAND                       ', 'L11 1AB ', '0151 284 4224       ', 2586879029, 1, 'AB111111'),
+('112', 12345672, 'LP', 'K', '                              ', '                  ', '                                                  ', 'Town B                         ', 'LIVERPOOL                     ', 'MERSEYSIDE', 'ENGLAND                       ', 'L11 2AB ', '                    ', 2586879028, 2, 'AB111111'),
+('113', 12345673, 'BL', 'N', 'FIRST LASTC                   ', 'PROBAT            ', '3, Street Road                                    ', 'Town C                         ', 'LIVERPOOL                     ', 'MERSEYSIDE', 'ENGLAND                       ', 'L11 3AB ', '                    ', 2586879027, 3, 'AB111111'),
+('114', 12345674, 'LI', 'H', 'FIRST LASTD                   ', 'PROBAT            ', '4, Street Road                                    ', 'Town D                         ', 'LIVERPOOL                     ', 'MERSEYSIDE', 'ENGLAND                       ', 'L11 4AB ', '                    ', 2586879026, 4, 'AB111112'),
+('115', 12345675, 'WS', 'S', 'FIRST LASTE                   ', 'WIFE              ', '5, Street Road                                    ', 'Town E                         ', 'LIVERPOOL                     ', 'MERSEYSIDE', 'ENGLAND                       ', 'L11 5AB ', '                    ', 2586879025, 5, 'AB111112'),
+('116', 12345676, 'PN', 'D', 'FIRST LASTF                   ', '                  ', '6, Street Road                                    ', 'Town F                         ', 'LIVERPOOL                     ', 'MERSEYSIDE', 'ENGLAND                       ', 'LA11 6AB', '0151 284 4224       ', 2586879024, 6, 'AB111112'),
+('117', 12345676, 'PN', ' ', 'FIRST LASTG                   ', '                  ', '7, Street Road                                    ', 'Town G                         ', 'LIVERPOOL                     ', 'MERSEYSIDE', 'ENGLAND                       ', 'LA11 7AB', '0151 284 4224       ', 2586879023, 7, 'AB111112'),
 
 
-('118', 12345676, 'HV', 'H', 'STEVEN SPIELBERG', 'PROBAT',  '1 Lone Pine Road', 'Lyon Estate ', 'Hill Valley ', 'California', 'USA ', 'LA11 7AB', '0151 284 4224       ', 2586879023, 'BF123451'),
-('119', 12345676, 'HV', 'S', 'MARVIN BERRY',     'UNCLE',   'Twin Pines Mall', 'Hill Valley', 'Hill Valley ',   'California', 'USA ', 'LA11 7AB', '0151 284 4224       ', 2586879023, 'BF123451'),
-('120', 12345676, 'HV', 'N', 'DAVE MCFLY',       'BROTHER', 'Twin Pines Ranch', 'Hill Valley', 'Hill Valley ',  'California', 'USA ', 'LA11 7AB', '0151 284 4224       ', 2586879023, 'BF123451'),
-('121', 12345676, 'HV', 'H', 'STEVEN SPIELBERG', 'PROBAT',  '1 Lone Pine Road', 'Lyon Estate ', 'Hill Valley ', 'California', 'USA ', 'LA11 7AB', '0151 284 4224       ', 2586879023, 'BF123452'),
-('122', 12345676, 'HV', 'S', 'MARVIN BERRY',     'UNCLE',   'Twin Pines Mall', 'Hill Valley', 'Hill Valley ',   'California', 'USA ', 'LA11 7AB', '0151 284 4224       ', 2586879023, 'BF123452'),
-('123', 12345676, 'HV', 'N', 'DAVE MCFLY',       'BROTHER', 'Twin Pines Ranch', 'Hill Valley', 'Hill Valley ',  'California', 'USA ', 'LA11 7AB', '0151 284 4224       ', 2586879023, 'BF123452'),
-('124', 12345676, 'HV', 'H', 'STEVEN SPIELBERG', 'PROBAT',  '1 Lone Pine Road', 'Lyon Estate ', 'Hill Valley ', 'California', 'USA ', 'LA11 7AB', '0151 284 4224       ', 2586879023, 'BF123453'),
-('125', 12345676, 'HV', 'S', 'MARVIN BERRY',     'UNCLE',   'Twin Pines Mall', 'Hill Valley', 'Hill Valley ',   'California', 'USA ', 'LA11 7AB', '0151 284 4224       ', 2586879023, 'BF123453'),
-('126', 12345676, 'HV', 'N', 'DAVE MCFLY',       'BROTHER', 'Twin Pines Ranch', 'Hill Valley', 'Hill Valley ',  'California', 'USA ', 'LA11 7AB', '0151 284 4224       ', 2586879023, 'BF123454')
+('118', 12345676, 'HV', 'H', 'STEVEN SPIELBERG', 'PROBAT',  '1 Lone Pine Road', 'Lyon Estate ', 'Hill Valley ', 'California', 'USA ', 'LA11 7AB', '0151 284 4224       ', 2586879023, 8, 'BF123451'),
+('119', 12345676, 'HV', 'S', 'MARVIN BERRY',     'UNCLE',   'Twin Pines Mall', 'Hill Valley', 'Hill Valley ',   'California', 'USA ', 'LA11 7AB', '0151 284 4224       ', 2586879023, 9, 'BF123451'),
+('120', 12345676, 'HV', 'N', 'DAVE MCFLY',       'BROTHER', 'Twin Pines Ranch', 'Hill Valley', 'Hill Valley ',  'California', 'USA ', 'LA11 7AB', '0151 284 4224       ', 2586879023, 10, 'BF123451'),
+('121', 12345676, 'HV', 'H', 'STEVEN SPIELBERG', 'PROBAT',  '1 Lone Pine Road', 'Lyon Estate ', 'Hill Valley ', 'California', 'USA ', 'LA11 7AB', '0151 284 4224       ', 2586879023, 11, 'BF123452'),
+('122', 12345676, 'HV', 'S', 'MARVIN BERRY',     'UNCLE',   'Twin Pines Mall', 'Hill Valley', 'Hill Valley ',   'California', 'USA ', 'LA11 7AB', '0151 284 4224       ', 2586879023, 12, 'BF123452'),
+('123', 12345676, 'HV', 'N', 'DAVE MCFLY',       'BROTHER', 'Twin Pines Ranch', 'Hill Valley', 'Hill Valley ',  'California', 'USA ', 'LA11 7AB', '0151 284 4224       ', 2586879023, 13, 'BF123452'),
+('124', 12345676, 'HV', 'H', 'STEVEN SPIELBERG', 'PROBAT',  '1 Lone Pine Road', 'Lyon Estate ', 'Hill Valley ', 'California', 'USA ', 'LA11 7AB', '0151 284 4224       ', 2586879023, 14, 'BF123453'),
+('125', 12345676, 'HV', 'S', 'MARVIN BERRY',     'UNCLE',   'Twin Pines Mall', 'Hill Valley', 'Hill Valley ',   'California', 'USA ', 'LA11 7AB', '0151 284 4224       ', 2586879023, 15, 'BF123453'),
+('126', 12345676, 'HV', 'N', 'DAVE MCFLY',       'BROTHER', 'Twin Pines Ranch', 'Hill Valley', 'Hill Valley ',  'California', 'USA ', 'LA11 7AB', '0151 284 4224       ', 2586879023, 16, 'BF123454')
 
 ;
 GO

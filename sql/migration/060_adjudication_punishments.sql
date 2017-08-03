983defaf5f983271162e953bc2ff0d603e4365ac
Alistair Todd <alistair.todd@digital.justice.gov.uk>
2017-08-03 16:11:41 +0100

    Seed data

--- sql/migration/060_adjudication_punishments.sql
+++ sql/migration/060_adjudication_punishments.sql
@@ -1,32 +1,41 @@
-
 -------------------------------------------------------------------------------------------------------
 
--- create new table to hold adjudications with punishments
-CREATE TABLE [HPA].[OFFENCES_IN_CUSTODY_PUNISHMENTS] (
-    [PK_OFFENCE_IN_CUSTODY] [INT]            NOT NULL IDENTITY (1, 1)
-        CONSTRAINT PK_OFFENCES_IN_CUSTODY PRIMARY KEY,
-    [ADJ_OFFENCE_ID]        [CHAR](26)       NOT NULL,
-    [PRISON_NUMBER]         [VARCHAR](8)     NOT NULL,
-    [DATE]                  [DATE]           NULL,
-    [CHARGE]                [VARCHAR](28)    NULL,
-    [OUTCOME]               [VARCHAR](28)    NULL,
-    [ESTABLISHMENT]         [VARCHAR](30)    NULL,
-    [PUNISHMENTS]           [NVARCHAR](2048) NULL,
+-- create new table to hold adjudications and punishments
+CREATE TABLE [HPA].[ADJUDICATIONS] (
+    [PK_ADJUDICATION] [INT]            NOT NULL IDENTITY (1, 1)
+        CONSTRAINT PK_ADJUDICATIONS PRIMARY KEY,
+    [ADJ_OFFENCE_ID]  [CHAR](26)       NOT NULL,
+    [PRISON_NUMBER]   [VARCHAR](8)     NOT NULL,
+    [DATE]            [DATE]           NULL,
+    [CHARGE]          [VARCHAR](28)    NULL,
+    [OUTCOME]         [VARCHAR](28)    NULL,
+    [ESTABLISHMENT]   [VARCHAR](30)    NULL,
+    [PUNISHMENTS]     [NVARCHAR](2048) NULL,
 );
+GO
+
+CREATE INDEX HPA_ADJUDICATIONS
+    ON HPA.ADJUDICATIONS
+    (
+        PRISON_NUMBER, DATE
+    ) INCLUDE (
+    CHARGE, OUTCOME, ESTABLISHMENT, PUNISHMENTS
+)
+    WITH (ONLINE = ON);
 GO
 
 -------------------------------------------------------------------------------------------------------
 
 -- ensure punishments column is JSON
-ALTER TABLE [HPA].[OFFENCES_IN_CUSTODY_PUNISHMENTS]
-    ADD CONSTRAINT [OFFENCES_IN_CUSTODY_PUNISHMENTS.PUNISHMENTS should be formatted as JSON]
+ALTER TABLE [HPA].[ADJUDICATIONS]
+    ADD CONSTRAINT [ADJUDICATIONS.PUNISHMENTS should be formatted as JSON]
 CHECK (ISJSON(PUNISHMENTS) > 0);
 GO
 
 -------------------------------------------------------------------------------------------------------
 
 -- populate from IIS
-INSERT INTO HPA.OFFENCES_IN_CUSTODY_PUNISHMENTS
+INSERT INTO HPA.ADJUDICATIONS
     SELECT
         o.PKTS_ADJ_OFFENCE                                                 AS ADJ_OFFENCE_ID,
         TRIM(l.PK_PRISON_NUMBER)                                           AS PRISON_NUMBER,
@@ -61,40 +70,43 @@ INSERT INTO HPA.OFFENCES_IN_CUSTODY_PUNISHMENTS
                    FROM IIS.IIS_IDENTIFIER i
                    WHERE i.FK_PERSON_IDENTIFIER = l.FK_PERSON_IDENTIFIER AND PERSON_IDENT_TYPE_CODE = 'NOM');
 GO
-
+--7m
 -------------------------------------------------------------------------------------------------------
 
 -- add new version of offences_in-Custody
 ALTER TABLE HPA.PRISONER_DETAILS
-    ADD [OFFENCES_IN_CUSTODY_PUNISHMENTS] [NVARCHAR](MAX) NULL;
+    ADD [ADJUDICATIONS] [NVARCHAR](MAX) NULL;
 GO
 
 -------------------------------------------------------------------------------------------------------
 
 -- ensure new version is JSON
 ALTER TABLE [HPA].[PRISONER_DETAILS]
-    ADD CONSTRAINT [PRISONER_DETAILS.OFFENCES_IN_CUSTODY_PUNISHMENTS should be formatted as JSON]
-CHECK (ISJSON(OFFENCES_IN_CUSTODY_PUNISHMENTS) > 0);
+    ADD CONSTRAINT [PRISONER_DETAILS.ADJUDICATIONS should be formatted as JSON]
+CHECK (ISJSON(ADJUDICATIONS) > 0);
 GO
-
+--15m
 -------------------------------------------------------------------------------------------------------
 
 -- populate new version with JSON
 UPDATE HPA.PRISONER_DETAILS
-SET OFFENCES_IN_CUSTODY_PUNISHMENTS =
+SET ADJUDICATIONS =
 (
     SELECT
-        DATE          AS date,
-        OUTCOME       AS outcome,
-        CHARGE        AS charge,
-        ESTABLISHMENT AS establishment,
-        PUNISHMENTS   AS punishments
-    FROM HPA.OFFENCES_IN_CUSTODY_PUNISHMENTS o
+        DATE                    AS date,
+        OUTCOME                 AS outcome,
+        CHARGE                  AS charge,
+        ESTABLISHMENT           AS establishment,
+        JSON_QUERY(PUNISHMENTS) AS punishments
+    FROM HPA.ADJUDICATIONS o
     WHERE o.PRISON_NUMBER = PK_PRISON_NUMBER
     ORDER BY DATE DESC
     FOR JSON PATH
 );
 GO
-
+--1h
 -------------------------------------------------------------------------------------------------------
--- not deleting original table and column yet in case of future need
+
+
+
+

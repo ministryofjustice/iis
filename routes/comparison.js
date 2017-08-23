2024f5b3683dec385f7715cbc7b3fc5c2601a8e0
mattwhitfield <whitfield.mj@gmail.com>
2017-08-23 13:54:10 +0100

    Added tabs in a jade mixin

--- routes/comparison.js
+++ routes/comparison.js
@@ -5,6 +5,9 @@ const express = require('express');
 // eslint-disable-next-line
 let router = express.Router();
 
+router.get('/', (req, res) => {
+    res.redirect(/search/);
+});
 router.get('/:prisonNumbers', getComparison);
 
 module.exports = router;

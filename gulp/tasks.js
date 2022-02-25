29a6d184b918dd1a167f0eaf105c271b20f0c65b
Neil Mendum <neil.mendum@digital.justice.gov.uk>
2022-02-25 16:36:01 +0000

    HAAR-177: convert gulp clean to npm script

--- gulp/tasks.js
+++ gulp/tasks.js
@@ -1,29 +1,18 @@
-'use strict';
+"use strict";
 
-let gulp = require('gulp');
-require('./webpack')
-require('./watch')
+let gulp = require("gulp");
+require("./webpack");
+require("./watch");
 
-gulp.task('watch', gulp.series(
-    'watch-sass',
-    'watch-assets',
-    'watch-client-js',
-    'watch-tests',
-));
+gulp.task(
+    "watch",
+    gulp.series("watch-sass", "watch-assets", "watch-client-js", "watch-tests")
+);
 
-gulp.task('generate-assets', gulp.series(
-        'copy-govuk-modules',
-        'sass',
-        'webpack',
-        'copy-assets'
-));
+gulp.task("generate-assets", gulp.series("sass", "webpack"));
 
-gulp.task('build', gulp.series('generate-assets'))
+gulp.task("build", gulp.series("generate-assets"));
 
-gulp.task('default', gulp.series('build'))
+gulp.task("default", gulp.series("build"));
 
-gulp.task('dev', gulp.series(
-        'build',
-        'watch',
-        'server',
-));
+gulp.task("dev", gulp.series("build", "watch", "server"));
